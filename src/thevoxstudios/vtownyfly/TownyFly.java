b009e96cbe6473c7435c7741293df60da47f029c
StuddedExpandable <studdedexpandable@gmail.com>
2016-05-29 00:03:13 +0100

    Fixed 90 percent of the EntityDamageEvent problem.

--- src/thevoxstudios/vtownyfly/TownyFly.java
+++ src/thevoxstudios/vtownyfly/TownyFly.java
@@ -70,6 +70,7 @@ public class TownyFly extends JavaPlugin implements Listener
 			config.set("Messages.Prefix", "&aTownyFly &8»");
 			config.set("Messages.FlyEnabled", "<prefix> &aTownyFly is now enabled!");
 			config.set("Messages.FlyDisabled", "<prefix> &aTownyFly is now disabled!");
+			config.set("Messages.NotJoinedTown", "<prefix> &cYou do not belong to this town, so you cannot fly in it!");
 			config.set("Messages.NotInATown", "<prefix> &cYou must be in your joined Town in order to use this command!");
 			config.set("Messages.NotInJoinedTown", "<prefix> &cYou are not in your joined Town! If you want to use this command you must be in the town you have joined.");
 			config.set("Messages.NoPermission", "<prefix> &cYou do not have permission to execute this command! &cWant to be able to execute this command? Become a registered &8[&aMember&]&a @ &dwww.thevoxmc.net &aand gain perks like this one!");
@@ -100,14 +101,23 @@ public class TownyFly extends JavaPlugin implements Listener
 		if (e.getEntity() instanceof Player)
 		{
 			Player p = (Player)e.getEntity();
-			if (e.getCause() == DamageCause.FALL && (p.hasPermission("vox.towny.fly") && (p.isFlying())))
+			if (e.getCause() == DamageCause.FALL && (p.hasPermission("vox.towny.fly")))
 			{
+				log("");
 				e.setCancelled(true);
 			}
-			if (p.getLastDamageCause().getCause() == DamageCause.FALL && (p.hasPermission("vox.towny.fly") && (p.isFlying())))
+			else
+			{
+				log("Damage not fall damage/Conditions not met");
+			}
+			if (p.getLastDamageCause().getCause() == DamageCause.FALL && (p.hasPermission("vox.towny.fly")))
 			{
 				e.setCancelled(true);
-			}			
+			}		
+			else
+			{
+				log("Damage not fall damage/Conditions not met x2");
+			}
 		}
 	}
 	
@@ -136,8 +146,8 @@ public class TownyFly extends JavaPlugin implements Listener
 					{
 							log("Resident "+ p + " has left their town.");
 						p.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Messages.OutOfTownBoundaries").replace("<prefix>", getConfig().getString("Messages.Prefix"))));
-						p.setFlying(false);
 						p.setAllowFlight(false);
+						p.setFlying(false);
 						tflyp.remove(p.getName());
 						return;
 					}
@@ -147,8 +157,8 @@ public class TownyFly extends JavaPlugin implements Listener
 			{
 				log("Resident has entered the wilderness.");
 				p.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Messages.OutOfTownBoundaries").replace("<prefix>", getConfig().getString("Messages.Prefix"))));
-				p.setFlying(false);
 				p.setAllowFlight(false);
+				p.setFlying(false);
 				tflyp.remove(p.getName());
 				return;
 			}
@@ -182,14 +192,13 @@ public class TownyFly extends JavaPlugin implements Listener
 						{
 							try 
 							{
-								if (res.getTown() != null) 
+								if (TownyUniverse.getTownBlock(p.getLocation()).getTown().equals(res.getTown()))
 								{
 									if (p.getAllowFlight()) 
 									{
 										tflyp.remove(p.getName());
-										p.setFlying(false);
 										p.setAllowFlight(false);
-										
+										p.setFlying(true);
 										p.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Messages.FlyDisabled").replace("<prefix>", getConfig().getString("Messages.Prefix"))));
 									} 
 									else 
@@ -199,6 +208,11 @@ public class TownyFly extends JavaPlugin implements Listener
 										p.setFlying(true);
 										p.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Messages.FlyEnabled").replace("<prefix>", getConfig().getString("Messages.Prefix"))));
 									}
+								}
+								else
+								{
+									p.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Messages.NotJoinedTown")));
+									log("User attempted to toggle /tfly outside of their town");
 								}
 							} 
 							catch (Exception e) 
@@ -212,7 +226,6 @@ public class TownyFly extends JavaPlugin implements Listener
 				{
 	        	   p.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Messages.NoPermission").replace("<prefix>", getConfig().getString("Messages.Prefix"))));
 	           }
-	
 	     } 
 			else 
 			{
